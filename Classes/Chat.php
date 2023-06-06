857096ad8c494d6a83a7771538a41c518697f5c5
Lukas Sliva <lukas.sliva.developer@gmail.com>
2023-06-06 14:13:30 +0200

    chat Updates

--- Classes/Chat.php
+++ Classes/Chat.php
@@ -47,34 +47,50 @@ class Chat {
         $statement->bindValue(':matchId', $matchId);
         $statement->execute();
     
+        $previousDate = null; // Variable to store the previous date
+
+
         while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
             $sender = $row['senderId'];
             $messageText = $row['message'];
             $timestamp = $row['timestamp'];
             // Create a DateTime object from the timestamp
             $date = new DateTime($timestamp);
+            $currentDate = $date->format('d/m/Y');
 
             // Format the DateTime object to display only the time
             $time = $date->format('H:i'); // 'H' represents 24-hour format, 'i' represents minutes, 's' represents seconds
-            echo "<div class='messageContent'>";
+               // Echo the date header if the current date is different from the previous date
+        if ($currentDate !== $previousDate) {
+            echo '<div class="date-header">' . $currentDate . '</div>';
+            $previousDate = $currentDate;
+        }
             echo '<div class="message">';
             if ($sender == $userId) {
                 echo '<div class="your-message">'; 
+                echo '<div class="messageText">';
                 echo '<span class="sender">You:</span><br>';
                 echo '<div class="text">' . $messageText . '</div>';
+                echo '</div>';
+                echo '<div class="infoDiv">';
+                echo "<i id='dots' class='bx bx-dots-vertical-rounded'><button><p id='delete'></p><p id='update'></p></button></i>";
                 echo '<span class="timestamp">' . $time . '</span>';
+                echo '</div>';
                 echo '</div>';
             } else if ($sender == $matchedUserId) {
                 echo '<div class="other-message">';
+                echo '<div class="messageText">';
                 echo '<span class="sender">' . $matchedUserName . ': </span><br>';
                 echo '<div class="text">' . $messageText . '</div>';
+                echo '</div>';
+                echo '<div class="infoDiv">';
+                echo "<i id='dots' class='bx bx-dots-vertical-rounded'><button><p id='delete'></p><p id='update'></p></button></i>";
+                // echo '<div id="delete">';
                 echo '<span class="timestamp">' . $time . '</span>';
-            echo '</div>';
-
+                echo '</div>';
+                echo '</div>';
             }
             echo '</div>';
-            echo '</div>';
-
         }
     }
     
--- chatForm.php
+++ chatForm.php
@@ -28,7 +28,7 @@
             }
             ?>
     <div class="chatForm">
-
+<div class="messageContent">
             <?php
             
 
@@ -39,14 +39,18 @@
             $chat1->readMessage($matchId, $userId, $matchedUserId, $matchedUserName);
 
         ?>
+        
         <div class="sendMessage">
-            <form action="send_message.php" method="post">
+            <form action="send_message.php" method="post" id="messageForm">
             <input type="hidden" name="matchId" value="<?php echo $matchId; ?>">
                 <input type="hidden" name="senderId" value="<?php echo $userId; ?>">
                 <input type="hidden" name="receiverId" value="<?php echo $matchedUserId; ?>">
-                <textarea name="message" placeholder="Enter your message" required></textarea>
+                <textarea id="message" name="message" placeholder="Enter your message" required></textarea>
+                <span id="charCount"></span>
                 <button type="submit">Send</button>
             </form>
+            
+        </div>
         </div>
 
 
@@ -59,60 +63,97 @@
         }
     ?>
     <script>
-        var chatForm = document.querySelector('.chatForm');
-chatForm.scrollTop = chatForm.scrollHeight;
+    const messageInput = document.getElementById('message');
+    const charCount = document.getElementById('charCount');
+    const messageForm = document.getElementById('messageForm');
 
+    messageInput.addEventListener('input', function () {
+        const messageLength = messageInput.value.length;
+        charCount.innerText = `${messageLength}/2000`;
+
+        if (messageLength > 2000) {
+            charCount.style.color = 'red';
+            messageForm.onsubmit = function(event) {
+                event.preventDefault();
+            };
+        } else {
+            charCount.style.color = 'black';
+            messageForm.onsubmit = null;
+        }
+    });
+        var chatForm = document.querySelector('.chatForm');
+        chatForm.scrollTop = chatForm.scrollHeight;
+        const dots = document.getElementById('dots');
+        const deleteElement = document.querySelector('delete');
+        const update = document.querySelector('update');
+        // const windowElement = document.getElementById('window');
+
+        dots.addEventListener('click', () => {
+        if(isClicked) {
+            deleteElement.innerHTML = '';
+            update.innerHTML = '';
+            dots.classList.remove('opened');
+            isClicked = false;
+            console.log($isClicked)
+        } else {
+            deleteElement.innerHTML = "<a href='deleteChat.php?action=chat&matchedUserId=' . $matchedUserId . ''>' . $matchedUserName . '</a>";
+            update.innerHTML = "<a href='deleteChat.php?action=chat&matchedUserId=' . $matchedUserId . ''>' . $matchedUserName . '</a>";
+            dots.classList.add('opened');
+            isClicked = true;
+        }
+        
+    });
         </script>
 <!-- <script>
-            function loadMessages() {
-            // Fetch chat messages from the database using AJAX
-            $.ajax({
-                url: 'fetch_messages.php',
-                method: 'GET',
-                dataType: 'json',
-                success: function(response) {
-                    // Display the chat messages on the page
-                    var chatContainer = $('#chat-container');
-                    chatContainer.empty();
+        //     function loadMessages() {
+        //     // Fetch chat messages from the database using AJAX
+        //     $.ajax({
+        //         url: 'fetch_messages.php',
+        //         method: 'GET',
+        //         dataType: 'json',
+        //         success: function(response) {
+        //             // Display the chat messages on the page
+        //             var chatContainer = $('#chat-container');
+        //             chatContainer.empty();
 
-                    for (var i = 0; i < response.length; i++) {
-                        var message = response[i];
-                        var sender = message.senderId;
-                        var messageText = message.message;
-                        var timestamp = message.timestamp;
+        //             for (var i = 0; i < response.length; i++) {
+        //                 var message = response[i];
+        //                 var sender = message.senderId;
+        //                 var messageText = message.message;
+        //                 var timestamp = message.timestamp;
 
-                        var messageDiv = $('<div class="message">');
-                        var senderSpan = $('<span class="sender">').text('Sender: ' + sender);
-                        var messageTextDiv = $('<div>').text(messageText);
-                        var timestampSpan = $('<span class="timestamp">').text('Timestamp: ' + timestamp);
+        //                 var messageDiv = $('<div class="message">');
+        //                 var senderSpan = $('<span class="sender">').text('Sender: ' + sender);
+        //                 var messageTextDiv = $('<div>').text(messageText);
+        //                 var timestampSpan = $('<span class="timestamp">').text('Timestamp: ' + timestamp);
 
-                        messageDiv.append(senderSpan, messageTextDiv, timestampSpan);
-                        chatContainer.append(messageDiv);
-                    }
+        //                 messageDiv.append(senderSpan, messageTextDiv, timestampSpan);
+        //                 chatContainer.append(messageDiv);
+        //             }
 
-                    // Scroll to the bottom of the chat container
-                    chatContainer.scrollTop(chatContainer[0].scrollHeight);
-                }
-            });
-        }
+        //             // Scroll to the bottom of the chat container
+        //             chatContainer.scrollTop(chatContainer[0].scrollHeight);
+        //         }
+        //     });
+        // }
 
-            // Function to send a chat message
-            function sendMessage() {
-            var messageInput = $('#message-input');
-            var chatMessage = messageInput.val();
+        //     // Function to send a chat message
+        //     function sendMessage() {
+        //     var messageInput = $('#message-input');
+        //     var chatMessage = messageInput.val();
 
-            // Send the chat message to the database using AJAX
-            $.ajax({
-                url: 'send_message.php',
-                method: 'POST',
-                data: { chatMessage: chatMessage },
-                success: function() {
-                    // Clear the input field and reload messages
-                    messageInput.val('');
-                    loadMessages();
-                }
-            });
-        }
+        //     // Send the chat message to the database using AJAX
+        //     $.ajax({
+        //         url: 'send_message.php',
+        //         method: 'POST',
+        //         data: { chatMessage: chatMessage },
+        //         success: function() {
+        //             // Clear the input field and reload messages
+        //             messageInput.val('');
+        //             loadMessages();
+        //         }
+        //     });
+        // }
 
 </script> -->
 <style>
@@ -121,7 +162,7 @@ chatForm.scrollTop = chatForm.scrollHeight;
         flex-direction: row;
         width: 100%;
     }
-        </style>
+</style>
 
     
 <?php require 'includes/footer.php'?>
